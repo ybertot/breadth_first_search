c51d14ed9a72c377c75dffc9a38921e335ea44b2
Yves Bertot <Yves.Bertot@inria.fr>
2023-02-24 13:52:09 +0100

    a second example, giving the possibility to instanciate the final map with an arbitrary map data structure.

--- bfs.v
+++ bfs.v
@@ -3,34 +3,35 @@ Require Import List.
 Section explore.
 
 Variable (state move : Type).
+Variable (state_fmap : Type).
+Variable find : state_fmap -> state -> option move.
+Variable add : state_fmap -> state -> move -> state_fmap.
 Variable (step : state -> list (state * move)).
 Variable (state_eq_dec : forall s1 s2 : state, {s1 = s2}+{s1 <> s2}).
 
 Definition bfs_aux 
-  (bfs' : list (state * move) -> list (state * move) ->
-          option (list (state * move)))  :
+  (bfs' : list (state * move) -> state_fmap -> option state_fmap)  :
   list (state * move) ->
   list (state * move) ->
-  list (state * move) ->
-  option (list (state * move)) :=
-fix bfs_aux (w w2 settled : list (state * move)) :
-  option (list (state * move)) :=
+  state_fmap ->
+  option state_fmap :=
+fix bfs_aux (w w2 : list (state * move)) (settled : state_fmap):
+  option state_fmap :=
 match w with
 | (s, m) :: w' =>
-  match find (fun p => if state_eq_dec s (fst p) then true else false) 
-             settled with
+  match find settled s with
   | Some _ => bfs_aux w' w2 settled
-  | None => bfs_aux w' (step s ++ w2) ((s, m) :: settled)
+  | None => bfs_aux w' (step s ++ w2) (add settled s m)
   end
 | nil =>
   match w2 with
-  | nil => Some(settled)
+  | nil => Some settled
   | _ => bfs' w2 settled
   end
 end.
 
-Fixpoint bfs (fuel : nat) (w settled : list (state * move)) :
-  option (list (state * move)) :=
+Fixpoint bfs (fuel : nat) (w : list (state * move)) (settled : state_fmap) :
+  option state_fmap :=
   match fuel with
   | 0 => None
   | S p => bfs_aux (bfs p) w nil settled
