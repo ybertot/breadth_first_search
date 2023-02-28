661f3b41c7f33e15b56edecc3a65036d67b7bde8
Yves Bertot <Yves.Bertot@inria.fr>
2023-02-28 08:22:39 +0100

    redesigned bfs to be more obviously terminal recursive

--- bfs.v
+++ bfs.v
@@ -9,33 +9,26 @@ Variable add : state_fmap -> state -> move -> state_fmap.
 Variable (step : state -> list (state * move)).
 Variable (state_eq_dec : forall s1 s2 : state, {s1 = s2}+{s1 <> s2}).
 
-Definition bfs_aux 
-  (bfs' : list (state * move) -> state_fmap ->
-               (state_fmap + (list (state * move) * state_fmap)))  :
-  list (state * move) ->
-  list (state * move) ->
-  state_fmap ->
-  (state_fmap + (list (state * move) * state_fmap)) :=
-fix bfs_aux (w w2 : list (state * move)) (settled : state_fmap):
-  state_fmap + (list (state * move) * state_fmap) :=
+Fixpoint bfs_aux (w w2 : list (state * move))
+  (settled : state_fmap) : (list (state * move) * state_fmap) :=
 match w with
 | (s, m) :: w' =>
   match find settled s with
   | Some _ => bfs_aux w' w2 settled
   | None => bfs_aux w' (step s ++ w2) (add settled s m)
   end
-| nil =>
-  match w2 with
-  | nil => inl settled
-  | _ => bfs' w2 settled
-  end
+| nil => (w2, settled)
 end.
 
 Fixpoint bfs (fuel : nat) (w : list (state * move)) (settled : state_fmap) :
   state_fmap + (list (state * move) * state_fmap) :=
   match fuel with
   | 0 => inr (w, settled)
-  | S p => bfs_aux (bfs p) w nil settled
+  | S p =>
+    match bfs_aux w nil settled with
+    | (nil, s) => inl s
+    | (w, s) => bfs p w s
+    end
   end.
 
 End explore.
